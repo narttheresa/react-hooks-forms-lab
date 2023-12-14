a5c5eb97f8b27294ea2c76310c51805578097395
Theresa Tran <theresa1194@gmail.com>
2023-12-14 11:39:41 +1100

    done

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({onItemFormSubmit}) {
+
+  const [newItem, SetNewItem] = useState({
+    id: uuid(),
+    name: "",
+    category: "produce",
+  })
+
+  function handleInputChange(event) {
+    const name = event.target.name
+    const value = event.target.value
+
+    SetNewItem({
+      ...newItem,
+      [name]: value
+    })
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    onItemFormSubmit(newItem)
+  }
+
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit} >
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={newItem.name} onChange={handleInputChange} />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" value={newItem.category} onChange={handleInputChange}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
